6590a0323ed84608d6668cb4084cfcc5871e760b
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-03-20 11:48:56 +0900

    Limitモデル作成

---
+++ database/migrations/2023_03_20_113958_create_limits_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLimitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('limits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('report_id')
+                ->constrained('report_categories')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->integer('limit_days');
+            $table->integer('limit_times')->nullable();
+            $table->timestamps();
+
+            // 複合ユニーク制約
+            $table->unique(['user_id', 'report_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('limits');
+    }
+}
