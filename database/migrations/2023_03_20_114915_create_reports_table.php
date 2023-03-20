0ea6b242679b46258da3d4ba1f292c802b07486d
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-03-20 12:05:37 +0900

    Reportモデル作成

---
+++ database/migrations/2023_03_20_114915_create_reports_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->date('report_date');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('report_id')
+                ->constrained('report_categories')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('reason_id')
+                ->constrained('reason_categories')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('reason_detail')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('remaining_days');
+            $table->integer('remaining_times')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
