f9db426d24b0d9d32a56858bf684755c45ef1ce7
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-05-18 17:02:27 +0900

    休暇失効alert機能

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,20 +10,26 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500&family=M+PLUS+Rounded+1c:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500&family=Courgette&family=M+PLUS+Rounded+1c:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <style>
+        .Courgette {
+            font-family: 'Courgette', cursive;
+        }
+
         .ZenMaruGothic {
             font-family: 'Zen Maru Gothic', sans-serif;
         }
-        .ZenKurenaido{
+
+        .ZenKurenaido {
             font-family: 'Zen Kurenaido', sans-serif;
         }
     </style>
@@ -34,7 +40,7 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-50">
-    {{-- <div class="min-h-screen bg-sky-50"> --}}
+        {{-- <div class="min-h-screen bg-sky-50"> --}}
         @include('layouts.navigation')
 
         <!-- Page Heading -->
