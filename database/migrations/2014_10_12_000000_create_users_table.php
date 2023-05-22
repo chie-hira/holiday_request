b290c06e5b127382979fa8277e302a30262aed53
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-05-22 11:50:48 +0900

    usersテーブルにemailカラム追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('employee')->nullable()->unique();
             $table->foreignId('factory_id') # メインの所属工場
