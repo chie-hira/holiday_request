566f2cc9554b15ff44a4dcf7c5422146a7a75d9e
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-09-02 00:27:33 +0900

    休暇取得状況修正

--- resources/views/components/acquisition-days.blade.php
+++ resources/views/components/acquisition-days.blade.php
@@ -1,16 +1,11 @@
 @props(['user', 'key'])
 
-{{-- @if ($user->sum_get_days->first()) --}}
-    @if ($user->acquisitionDays($key) != 0)
-        {{ $user->acquisitionDays($key) }} 日
-    @endif
-    @if ($user->acquisitionHours($key) != 0)
-        {{ $user->acquisitionHours($key) }} 時間
-    @endif
+@if ($key <= 2 || $key == 12)
+    {{ $user->acquisitionDays($key) }} 日
+@endif
+@if ($key == 13 || $key == 14 || $key == 15)
+    {{ $user->acquisitionHours($key) }} 時間
     @if ($user->acquisitionMinutes($key) != 0)
         {{ $user->acquisitionMinutes($key) }} 分
     @endif
-    @if ($user->acquisitionDays($key) == 0)
-        {{ 0 }} 日
-    @endif
-{{-- @endif --}}
+@endif
